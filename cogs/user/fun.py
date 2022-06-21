1202ebdf77786ebf03de8e3470ee8a17cf1bc904
Werdor <ptenplay@gmail.com>
2022-06-21 15:16:56 +0500

    Add "avatar" user command

---
+++ cogs/user/fun.py
@@ -0,0 +1,26 @@
+from disnake.ext import commands
+import disnake
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+dACI = 'disnake.ApplicationCommandInteraction'
+
+class UserFunCommand(commands.Cog):
+    
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.user_command(name='Avatar')
+    async def avatar(self,ctx,member: disnake.Member):
+        if member.avatar == None:
+            emb = disnake.Embed(title='⚠️ The user has no avatar.',color=0xe36f02)
+            await ctx.response.send_message(embed=emb,ephemeral=True)
+        else:
+            emb = disnake.Embed(title=f'{member}',color=0xf7e645)
+            emb.set_image(url=member.avatar)
+            await ctx.response.send_message(embed=emb,ephemeral=True)
+
+def setup(bot: commands.Bot):
+    bot.add_cog(UserFunCommand(bot))
