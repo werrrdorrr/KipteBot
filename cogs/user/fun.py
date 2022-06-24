956df599536ab0393b277be9acd32f66be5a98e7
Werdor <ptenplay@gmail.com>
2022-06-24 13:01:19 +0500

    Update "avatar" user command

--- cogs/user/fun.py
+++ cogs/user/fun.py
@@ -15,17 +15,13 @@ class UserFunCommand(commands.Cog):
     @commands.user_command(name='Avatar')
     async def avatar(
         self,
-        ctx,
+        ctx: dACI,
         member: disnake.Member
         ):
         
-        if member.avatar == None:
-            emb = disnake.Embed(title='⚠️ The user has no avatar.',color=0xe36f02)
-            await ctx.response.send_message(embed=emb,ephemeral=True)
-        else:
-            emb = disnake.Embed(title=f'{member}',color=0xf7e645)
-            emb.set_image(url=member.avatar)
-            await ctx.response.send_message(embed=emb,ephemeral=True)
+        emb = disnake.Embed(title=f'{member}',color=0xf7e645)
+        emb.set_image(url=member.display_avatar)
+        await ctx.response.send_message(embed=emb,ephemeral=True)
 
 def setup(bot: commands.Bot):
     bot.add_cog(UserFunCommand(bot))
