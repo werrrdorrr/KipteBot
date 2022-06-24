b753a641c9fdaba7f8e2bf30ddcf58c81ea52a7e
Werdor <ptenplay@gmail.com>
2022-06-24 13:00:18 +0500

    Minor update

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-bot = commands.Bot(guild_ids=[986294560365903933],reload=True,sync_commands_debug=True)
+
+bot = commands.Bot(test_guilds=[986294560365903933],reload=True,sync_commands_debug=True)
+
+for ext in ['cogs.slash.fun','cogs.slash.admin','cogs.user.fun']:  
+    bot.load_extension(ext)
 
 print('Connecting to Discord servers...')
 
 @bot.event
 async def on_ready():
     print("The bot is ready!")
-
-for ext in ['cogs.slash.fun','cogs.slash.admin','cogs.user.fun']:  
-    bot.load_extension(ext)
-
-
+    
 bot.run(os.getenv('TOKEN'))
