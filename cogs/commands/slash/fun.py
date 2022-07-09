6aab2c63cf5f6decd49dc7f75724a0493cddd0fb
Werdor <ptenplay@gmail.com>
2022-07-10 01:39:11 +0500

    Update "8ball" slash command

--- cogs/commands/slash/fun.py
+++ cogs/commands/slash/fun.py
@@ -57,12 +57,12 @@ class SlashFunCommand(commands.Cog):
     @fun.sub_command(name = '8ball',description = 'Magic 8 ball')
     async def ball(
         self,
-        ball_answers=["It is certain.","It is decidedly so.","Without a doubt.","Yes definitely.","You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
         ctx: dACI,
         question: str = commands.Param(
             description = 'What do you want to ask?'
         )
     ):
+        from data.commands.slash.fun import ball_answers
         await ctx.response.defer(ephemeral = True)
         emb = disnake.Embed(
             title = '🔮 8ball',
