65b697310ea962e4dacf60e2734a740761242764
Werdor <ptenplay@gmail.com>
2022-07-18 03:21:44 +0500

    Add "youtube" slash command

--- cogs/commands/slash/party.py
+++ cogs/commands/slash/party.py
@@ -18,6 +18,22 @@ class SlashPartyCommand(commands.Cog):
     ):
         pass
 
+    @party.sub_command(description = 'Watch together YouTube')
+    async def youtube(
+        self,
+        ctx,
+        voice: disnake.VoiceChannel = commands.Param(
+            description = 'Select the voice channel you want'
+        )
+    ):
+        await ctx.response.defer()
+        invite = await voice.create_invite(
+            target_type = disnake.InviteTarget.embedded_application, 
+            target_application = disnake.PartyType.watch_together,
+            max_age = 300
+        )
+        await ctx.edit_original_message(f'[Click me]({invite}) (The link is valid for 5 minutes)')
+
 def setup(
     bot: commands.Bot
 ):
